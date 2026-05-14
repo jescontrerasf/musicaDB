c4b4c8afbbd9c6d3186be168332bedf8e13df539
jescontrerasf <jes.contrerasf@duocuc.cl>
2026-05-13 21:09:49 -0400

    proyecto Base Musica

---
+++ musicaDB/src/main/java/com/example/musicaDB/MusicaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicaDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaDbApplication.class, args);
+	}
+
+}
---
+++ musicaDB/src/test/java/com/example/musicaDB/MusicaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicaDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
